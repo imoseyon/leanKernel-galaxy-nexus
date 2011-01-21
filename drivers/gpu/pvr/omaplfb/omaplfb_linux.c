f60a4091d8499f69fd16114b23756b8fbad3f633
Gustavo Diaz Prado <x0083741@ti.com>
2011-01-21 15:36:37 -0600

    SGX: UDD: Changing the early suspend registration level
    This patch fixes a problem when a native application is running
    on the secondary display at the same time the Android UI is being
    shown in the primary display and the suspend mechanism happens.
    
    By registering to EARLY_SUSPEND_LEVEL_STOP_DRAWING level the
    UDD will receive the suspend signal before the DSS goes to suspend,
    this prevents UDD to interact with the DSS when it is already
    suspended. The resume mechanism will work in the inverse way,
    the DSS will be notified first that it needs to resume, then
    the UDD will be allowed to push frames again to the DSS.
    
    This ensures the UDD receives the suspend/resume signal at the
    correct time.
    
    Signed-off-by: Gustavo Diaz Prado <x0083741@ti.com>
    Change-Id: Ie634e17f6300c3ff2ea9973c36563c6565483322

--- drivers/gpu/pvr/omaplfb/omaplfb_linux.c
+++ drivers/gpu/pvr/omaplfb/omaplfb_linux.c
@@ -405,7 +405,7 @@ static struct platform_driver omaplfb_driver = {
 static struct early_suspend omaplfb_early_suspend = {
 	.suspend = OMAPLFBDriverSuspend_Entry,
 	.resume = OMAPLFBDriverResume_Entry,
-	.level = EARLY_SUSPEND_LEVEL_DISABLE_FB,
+	.level = EARLY_SUSPEND_LEVEL_STOP_DRAWING,
 };
 
 #else /* defined(SGX_EARLYSUSPEND) && defined(CONFIG_HAS_EARLYSUSPEND) */
