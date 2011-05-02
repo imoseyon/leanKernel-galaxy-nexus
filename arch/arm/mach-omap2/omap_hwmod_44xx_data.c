836f6a62ad58f7974de7ab4c7a13a73d499afadf
Avinash.H.M <avinashhm@ti.com>
2011-05-02 21:04:55 +0530

    omap4: enable, disable GPIO device
    Enable and disable the GPIO device in the resume and prepare idle hooks for
    GPIO. Without this clocks won't be disabled and it won't transition to
    lowpower.
    
    Signed-off-by: Avinash.H.M <avinashhm@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_44xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_44xx_data.c
@@ -1838,7 +1838,8 @@ static struct omap_hwmod_opt_clk gpio2_opt_clks[] = {
 static struct omap_hwmod omap44xx_gpio2_hwmod = {
 	.name		= "gpio2",
 	.class		= &omap44xx_gpio_hwmod_class,
-	.flags		= HWMOD_CONTROL_OPT_CLKS_IN_RESET,
+	.flags		= HWMOD_CONTROL_OPT_CLKS_IN_RESET |
+			HWMOD_SWSUP_SIDLE ,
 	.mpu_irqs	= omap44xx_gpio2_irqs,
 	.mpu_irqs_cnt	= ARRAY_SIZE(omap44xx_gpio2_irqs),
 	.main_clk	= "gpio2_ick",
