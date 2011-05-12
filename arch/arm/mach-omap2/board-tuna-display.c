e45fe58a23202b166f44bdb3b267b819307a65af
Colin Cross <ccross@android.com>
2011-05-12 14:36:37 -0700

    ARM: omap4: tuna: Move display mux settings to display init
    Change-Id: I0b7d458a73f60a820e12a594733188c2dcd8c6f5
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/mach-omap2/board-tuna-display.c
+++ arch/arm/mach-omap2/board-tuna-display.c
@@ -20,6 +20,9 @@
 #include <plat/panel-generic-dpi.h>
 #include <plat/vram.h>
 
+#include "board-tuna.h"
+#include "mux.h"
+
 #define TUNA_FB_RAM_SIZE		SZ_8M /* ~1280*720*4 * 2 */
 
 static struct panel_generic_dpi_data tuna_lcd_panel = {
@@ -58,8 +61,45 @@ static struct omapfb_platform_data tuna_fb_pdata = {
 	},
 };
 
+#define MUX_DISPLAY_OUT OMAP_PIN_OUTPUT | OMAP_MUX_MODE5
 void __init omap4_tuna_display_init(void)
 {
+	if (omap4_tuna_get_revision() == TUNA_REV_PRE_LUNCHBOX) {
+		/* dispc2_data23 - dispc2_data0 */
+		omap_mux_init_signal("usbb2_ulpitll_stp", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_dir", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_nxt", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_dat0", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_dat1", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_dat2", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu6", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu5", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_dat3", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_dat4", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_dat5", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_dat6", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("usbb2_ulpitll_dat7", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu3", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu4", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu11", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu12", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu13", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu14", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu15", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu16", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu17", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu18", MUX_DISPLAY_OUT);
+		omap_mux_init_signal("dpm_emu19", MUX_DISPLAY_OUT);
+		/* dispc2_hsync */
+		omap_mux_init_signal("dpm_emu7", MUX_DISPLAY_OUT);
+		/* dispc2_pclk */
+		omap_mux_init_signal("dpm_emu8", MUX_DISPLAY_OUT);
+		/* dispc2_vsync */
+		omap_mux_init_signal("dpm_emu9", MUX_DISPLAY_OUT);
+		/* dispc2_de */
+		omap_mux_init_signal("dpm_emu10", MUX_DISPLAY_OUT);
+	}
+
 	omap_vram_set_sdram_vram(TUNA_FB_RAM_SIZE, 0);
 	omapfb_set_platform_data(&tuna_fb_pdata);
 	omap_display_init(&tuna_dss_data);
