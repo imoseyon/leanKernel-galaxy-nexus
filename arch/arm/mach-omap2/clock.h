cde3c2d61b5b4a1e8ff644117a078518d719b0c1
Rajendra Nayak <rnayak@ti.com>
2011-05-26 14:42:20 +0530

    OMAP4: clock: wait for module to become accessible on a clk enable
    On omap4, the clock f/w is missing a check for module accessebility
    on a call to clk enable. Since this check is already part of the
    omap device layer, this check at the clock framework is redundant.
    But since not all drivers are yet using omap device apis, and most still
    depend on clk framework to enable clocks, this patch seems to be needed
    at this point to avoid any module accessebility issues
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>

--- arch/arm/mach-omap2/clock.h
+++ arch/arm/mach-omap2/clock.h
@@ -135,6 +135,7 @@ extern u8 cpu_mask;
 extern const struct clkops clkops_omap2_dflt_wait;
 extern const struct clkops clkops_dummy;
 extern const struct clkops clkops_omap2_dflt;
+extern const struct clkops clkops_omap4_dflt_wait;
 
 extern struct clk_functions omap2_clk_functions;
 extern struct clk *vclk, *sclk;
