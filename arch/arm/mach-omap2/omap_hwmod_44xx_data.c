b58901bf1f57b7bacf4a5d76dfd085914ddced6a
Axel Haslam <axelhaslam@ti.com>
2011-06-02 22:38:23 -0500

    HACK: OMAP4: hwmod: Dont enable idle for aess
    On previous kernels, wrong hwmod register addres was preventing
    smartidle mode from being enabled. Now that the register
    addres is corrected and when Smartidle is set, ABE pwrdm
    does not hit retention, preventing system from hitting CSWR.
    
    This issue is being root caused by TI hardware team.
    
    Acked-by: Misael Lopez <misael.lopez@ti.com>
    Signed-off-by: Axel Haslam <axelhaslam@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_44xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_44xx_data.c
@@ -885,7 +885,7 @@ static struct omap_hwmod omap44xx_smartreflex_mpu_hwmod = {
 static struct omap_hwmod_class_sysconfig omap44xx_aess_sysc = {
 	.rev_offs	= 0x0000,
 	.sysc_offs	= 0x0010,
-	.sysc_flags	= (SYSC_HAS_MIDLEMODE | SYSC_HAS_SIDLEMODE),
+	.sysc_flags	= 0,
 	.idlemodes	= (SIDLE_FORCE | SIDLE_NO | SIDLE_SMART |
 			   MSTANDBY_FORCE | MSTANDBY_NO | MSTANDBY_SMART),
 	.sysc_fields	= &omap_hwmod_sysc_type2,
