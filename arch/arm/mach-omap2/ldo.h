f856c4cd7a4926d3ebfb08fb778a2374f857b9b7
Mike Turquette <mturquette@ti.com>
2011-06-24 07:12:44 -0500

    OMAP3630: ABB: Add Adaptive Body-Bias LDO data
    This introduces the data needed to represent the ABB LDOs in the
    appropriate support for OMAP3630.
    
    Note that OMAP34XX does not have this LDO, so any data for
    that chip has been initialized as zero.
    
    [nm@ti.com: modifications]
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Mike Turquette <mturquette@ti.com>

--- arch/arm/mach-omap2/ldo.h
+++ arch/arm/mach-omap2/ldo.h
@@ -99,6 +99,8 @@ struct omap_ldo_abb_instance {
 	int __cur_abb_type;
 };
 
+extern struct omap_ldo_abb_instance omap3630_ldo_abb_mpu_instance;
+
 extern int omap_ldo_abb_pre_scale(struct voltagedomain *voltdm,
 			   unsigned long target_volt);
 extern int omap_ldo_abb_post_scale(struct voltagedomain *voltdm,
