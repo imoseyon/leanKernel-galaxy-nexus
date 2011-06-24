2459b9708da3c6025896c6aeb67dc37383eaf6bf
Rom Lemarchand <rlemarchand@sta.samsung.com>
2011-06-24 16:29:31 +0900

    ARM: omap4: tuna: Add vibrator driver
    Add vibrator driver for the tuna board.
    
    Change-ID: I677f73c001f5c8b03f6806ec7262a6a38b94d857
    Signed-off-by: Rom Lemarchand <rlemarchand@sta.samsung.com>

---
+++ arch/arm/mach-omap2/board-tuna-vibrator.c
@@ -0,0 +1,178 @@
+/* arch/arm/mach-omap2/board-tuna-vibrator.c
+ *
+ * Copyright (C) 2011 Samsung Electronics Co. Ltd. All Rights Reserved.
+ * Author: Rom Lemarchand <rlemarchand@sta.samsung.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/hrtimer.h>
+#include <linux/gpio.h>
+#include <linux/wakelock.h>
+#include <linux/mutex.h>
+#include <asm/mach-types.h>
+#include <plat/dmtimer.h>
+
+#include <../../../drivers/staging/android/timed_output.h>
+
+#include "mux.h"
+#include "board-tuna.h"
+
+#define GPIO_MOTOR_EN		162
+#define VIB_GPTIMER_NUM		10
+#define PWM_DUTY_MAX		1450
+#define MAX_TIMEOUT		10000 /* 10s */
+
+static struct vibrator {
+	struct wake_lock wklock;
+	struct hrtimer timer;
+	struct mutex lock;
+	struct omap_dm_timer *gptimer;
+	bool enabled;
+} vibdata;
+
+static void vibrator_off(void)
+{
+	if (!vibdata.enabled)
+		return;
+	omap_dm_timer_stop(vibdata.gptimer);
+	gpio_set_value(GPIO_MOTOR_EN, 0);
+	vibdata.enabled = false;
+	wake_unlock(&vibdata.wklock);
+}
+
+static int vibrator_get_time(struct timed_output_dev *dev)
+{
+	if (hrtimer_active(&vibdata.timer)) {
+		ktime_t r = hrtimer_get_remaining(&vibdata.timer);
+		return ktime_to_ms(r);
+	}
+
+	return 0;
+}
+
+static void vibrator_enable(struct timed_output_dev *dev, int value)
+{
+	mutex_lock(&vibdata.lock);
+
+	/* cancel previous timer and set GPIO according to value */
+	hrtimer_cancel(&vibdata.timer);
+
+	if (value) {
+		wake_lock(&vibdata.wklock);
+
+		gpio_set_value(GPIO_MOTOR_EN, 1);
+		omap_dm_timer_start(vibdata.gptimer);
+
+		vibdata.enabled = true;
+
+		if (value > 0) {
+			if (value > MAX_TIMEOUT)
+				value = MAX_TIMEOUT;
+
+			hrtimer_start(&vibdata.timer,
+				ns_to_ktime((u64)value * NSEC_PER_MSEC),
+				HRTIMER_MODE_REL);
+		}
+	} else {
+		vibrator_off();
+	}
+
+	mutex_unlock(&vibdata.lock);
+}
+
+static struct timed_output_dev to_dev = {
+	.name		= "vibrator",
+	.get_time	= vibrator_get_time,
+	.enable		= vibrator_enable,
+};
+
+static enum hrtimer_restart vibrator_timer_func(struct hrtimer *timer)
+{
+	vibrator_off();
+	return HRTIMER_NORESTART;
+}
+
+static int __init vibrator_init(void)
+{
+	int ret;
+
+	vibdata.enabled = false;
+
+	hrtimer_init(&vibdata.timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	vibdata.timer.function = vibrator_timer_func;
+
+	vibdata.gptimer = omap_dm_timer_request_specific(VIB_GPTIMER_NUM);
+	if (vibdata.gptimer == NULL)
+		return -1;
+
+	ret = omap_dm_timer_set_source(vibdata.gptimer,
+		OMAP_TIMER_SRC_SYS_CLK);
+	if (ret < 0)
+		goto err_dm_timer_src;
+
+	omap_dm_timer_set_load(vibdata.gptimer, 1, -PWM_DUTY_MAX);
+	omap_dm_timer_set_match(vibdata.gptimer, 1, -PWM_DUTY_MAX+10);
+	omap_dm_timer_set_pwm(vibdata.gptimer, 0, 1,
+		OMAP_TIMER_TRIGGER_OVERFLOW_AND_COMPARE);
+	omap_dm_timer_enable(vibdata.gptimer);
+	omap_dm_timer_write_counter(vibdata.gptimer, -2);
+	omap_dm_timer_disable(vibdata.gptimer);
+
+	wake_lock_init(&vibdata.wklock, WAKE_LOCK_SUSPEND, "vibrator");
+	mutex_init(&vibdata.lock);
+
+	ret = timed_output_dev_register(&to_dev);
+	if (ret < 0)
+		goto err_to_dev_reg;
+
+	return 0;
+
+err_to_dev_reg:
+	mutex_destroy(&vibdata.lock);
+	wake_lock_destroy(&vibdata.wklock);
+
+err_dm_timer_src:
+	omap_dm_timer_free(vibdata.gptimer);
+	vibdata.gptimer = NULL;
+
+	return -1;
+}
+
+static int __init omap4_tuna_vibrator_init(void)
+{
+	int ret;
+
+	if (!machine_is_tuna())
+		return 0;
+
+	omap_mux_init_gpio(GPIO_MOTOR_EN, OMAP_PIN_OUTPUT |
+						OMAP_PIN_OFF_OUTPUT_LOW);
+	omap_mux_init_signal("dpm_emu18.dmtimer10_pwm_evt", OMAP_PIN_OUTPUT);
+
+	ret = gpio_request(GPIO_MOTOR_EN, "vibrator-en");
+	if (ret)
+		return ret;
+
+	gpio_direction_output(GPIO_MOTOR_EN, 0);
+
+	ret = vibrator_init();
+	if (ret < 0)
+		gpio_free(GPIO_MOTOR_EN);
+
+	return ret;
+}
+
+/*
+ * This is needed because the vibrator is dependent on omap_dm_timers which get
+ * initialized at device_init time
+ */
+late_initcall(omap4_tuna_vibrator_init);
