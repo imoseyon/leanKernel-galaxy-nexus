fc1dec3f80bdf1c9c184079aabbee0d0145f6520
Santosh Shilimkar <santosh.shilimkar@ti.com>
2011-06-24 20:31:38 -0500

    OMAP4: clocks: Add CORE M2 set rate support
    This patch adds support for modifying the CORE DPLL M2 value
    using the shadow registers. The API programs the CM shadow
    registers to update CORE DPLL M2 divider. M2 divider is used to clock
    external DDR and its reconfiguration on frequency change is managed
    through a hardware sequencer. This is managed by the PRCM with EMIF
    using shadow registers.
    
    [girishsg@ti.com: Helped in fixing comments]
    Signed-off-by: Girish S G <girishsg@ti.com>
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

--- arch/arm/mach-omap2/clock44xx.h
+++ arch/arm/mach-omap2/clock44xx.h
@@ -16,5 +16,6 @@
 #define OMAP4430_MAX_DPLL_DIV	128
 
 int omap4xxx_clk_init(void);
+int omap4_core_dpll_m2_set_rate(struct clk *clk, unsigned long rate);
 
 #endif
--- arch/arm/mach-omap2/clock44xx_data.c
+++ arch/arm/mach-omap2/clock44xx_data.c
@@ -505,7 +505,7 @@ static struct clk dpll_core_m2_ck = {
 	.ops		= &clkops_omap4_dpllmx_ops,
 	.recalc		= &omap2_clksel_recalc,
 	.round_rate	= &omap2_clksel_round_rate,
-	.set_rate	= &omap2_clksel_set_rate,
+	.set_rate	= &omap4_core_dpll_m2_set_rate,
 };
 
 static struct clk ddrphy_ck = {
