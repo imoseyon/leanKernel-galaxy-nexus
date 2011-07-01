85255805d902a9f447ffd5d715deefb668152981
Dmitry Shmidt <dimitrysh@google.com>
2011-07-01 13:51:11 -0700

    mmc: omap_hsmmc: Skip mmc suspend/resume ops for SDIO device
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -2361,7 +2361,8 @@ static int omap_hsmmc_suspend(struct device *dev)
 			}
 		}
 		cancel_work_sync(&host->mmc_carddetect_work);
-		ret = mmc_suspend_host(host->mmc);
+		if (host->mmc->card && (host->mmc->card->type != MMC_TYPE_SDIO))
+			ret = mmc_suspend_host(host->mmc);
 		mmc_host_enable(host->mmc);
 		if (ret == 0) {
 			omap_hsmmc_disable_irq(host);
@@ -2422,7 +2423,8 @@ static int omap_hsmmc_resume(struct device *dev)
 		omap_hsmmc_protect_card(host);
 
 		/* Notify the core to resume the host */
-		ret = mmc_resume_host(host->mmc);
+		if (host->mmc->card && (host->mmc->card->type != MMC_TYPE_SDIO))
+			ret = mmc_resume_host(host->mmc);
 		if (ret == 0)
 			host->suspended = 0;
 
