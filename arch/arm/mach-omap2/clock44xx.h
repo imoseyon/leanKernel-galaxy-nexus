33615ca6820f281c3b15edb792385c8704123bd9
Vishwanath BS <vishwanath.bs@ti.com>
2011-07-05 12:37:13 -0700

    OMAP4 Clock: Update sequence for dpll core m5x2 clock
    Following sequence has to be followed while changing dpll core m5x2 clock if
    GPMC does not support on the fly l3 frequency change.
    - set DPLL_CORE_M5_DIV with new value of M5 post-divider on L3 clock generation path
    - set CLKSEL_L3=1 (unchanged)
    - set CLKSEL_CORE=0 (unchanged)
    - set GPMC_FREQ_UPDATE=1
    Write the above value into CM_SHADOW_FREQ_CONFIG2.
    
    Since it's not known at board level as to what is connected to GPMC, it's safer to apply
    this sequence unconditionally.
    
    [rnayak@ti.com: Added the missing wait after update]
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/mach-omap2/clock44xx.h
+++ arch/arm/mach-omap2/clock44xx.h
@@ -18,5 +18,7 @@
 
 int omap4xxx_clk_init(void);
 int omap4_core_dpll_m2_set_rate(struct clk *clk, unsigned long rate);
+int omap4_core_dpll_m5x2_set_rate(struct clk *clk, unsigned long rate);
+
 
 #endif
