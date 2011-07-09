20e656b853014801a9618e3ae14d38e78a8ce998
Misael Lopez Cruz <misael.lopez@ti.com>
2011-07-08 21:28:51 -0500

    OMAP4: OPP: Add OPPs for AESS
    Add OPPs for Audio Engine Subsystem.
    
    Acked-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -142,7 +142,10 @@ static struct omap_opp_def __initdata omap443x_opp_def_list[] = {
 	/* DSP OPP3 - OPPTB */
 	OPP_INITIALIZER("dsp", "dpll_iva_m4x2_ck", "iva", false, 496000000, OMAP4430_VDD_IVA_OPPTURBO_UV),
 
-	/* TODO: add aess */
+	/* ABE OPP1 - OPP50 */
+	OPP_INITIALIZER("aess", "abe_clk", "iva", true, 98304000, OMAP4430_VDD_IVA_OPP50_UV),
+	/* ABE OPP2 - OPP100 */
+	OPP_INITIALIZER("aess", "abe_clk", "iva", true, 196608000, OMAP4430_VDD_IVA_OPP100_UV),
 };
 
 #define OMAP4460_VDD_MPU_OPP50_UV		1025000
@@ -271,7 +274,10 @@ static struct omap_opp_def __initdata omap446x_opp_def_list[] = {
 	/* DSP OPP3 - OPPTB */
 	OPP_INITIALIZER("dsp", "dpll_iva_m4x2_ck", "iva", false, 496000000, OMAP4460_VDD_IVA_OPPTURBO_UV),
 
-	/* TODO: add aess */
+	/* ABE OPP1 - OPP50 */
+	OPP_INITIALIZER("aess", "abe_clk", "iva", true, 98304000, OMAP4460_VDD_IVA_OPP50_UV),
+	/* ABE OPP2 - OPP100 */
+	OPP_INITIALIZER("aess", "abe_clk", "iva", true, 196608000, OMAP4460_VDD_IVA_OPP100_UV),
 };
 
 /**
