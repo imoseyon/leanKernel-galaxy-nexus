555816c844b2d3e65442302885c194dced174fc6
Todd Poynor <toddpoynor@google.com>
2011-07-19 15:47:53 -0700

    power: max17040: Fix crash on race scheduling worker at probe
    Initialize the worker before calling power_supply_register().  The
    external_power_changed callback may be called immediately by
    power_supply, which will schedule the worker, prior to initializing
    the worker.  The uninitialized data structure can trigger a BUG in
    the workqueue code, causing the worker thread to die.
    
    Also move the reset prior to power_supply_register(), to ensure
    the worker isn't run prior to, or concurrent with, the reset.
    
    Change-Id: I969cc04dc6eb4579b8b91f788383addeb7f8d129
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- drivers/power/max17040_battery.c
+++ drivers/power/max17040_battery.c
@@ -250,6 +250,9 @@ static int __devinit max17040_probe(struct i2c_client *client,
 	chip->battery.num_properties	= ARRAY_SIZE(max17040_battery_props);
 	chip->battery.external_power_changed	= max17040_ext_power_changed;
 
+	max17040_reset(client);
+	max17040_get_version(client);
+	INIT_DELAYED_WORK_DEFERRABLE(&chip->work, max17040_work);
 	ret = power_supply_register(&client->dev, &chip->battery);
 	if (ret) {
 		dev_err(&client->dev, "failed: power supply register\n");
@@ -257,10 +260,6 @@ static int __devinit max17040_probe(struct i2c_client *client,
 		return ret;
 	}
 
-	max17040_reset(client);
-	max17040_get_version(client);
-
-	INIT_DELAYED_WORK_DEFERRABLE(&chip->work, max17040_work);
 	schedule_delayed_work(&chip->work, msecs_to_jiffies(MAX17040_DELAY));
 
 	return 0;
