233afb8b2ed79da64b8ddad7f99892c5abec02d7
Erik Gilling <konkers@android.com>
2011-07-21 15:32:39 -0700

    OMAP4: DSSCOMP: move early suspend to after STOP_DRAWING
    Change-Id: Ie04e3bcae4aa23a9c11f37347d110e61a141f9fb
    Signed-off-by: Erik Gilling <konkers@android.com>

--- drivers/video/omap2/dsscomp/gralloc.c
+++ drivers/video/omap2/dsscomp/gralloc.c
@@ -300,7 +300,7 @@ static void dsscomp_late_resume(struct early_suspend *h)
 static struct early_suspend early_suspend_info = {
 	.suspend = dsscomp_early_suspend,
 	.resume = dsscomp_late_resume,
-	.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
+	.level = EARLY_SUSPEND_LEVEL_DISABLE_FB,
 };
 #endif
 
