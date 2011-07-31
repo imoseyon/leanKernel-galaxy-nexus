9cb431e70c157363cdbfc4fce3cddf6b5c0c54c2
Nishanth Menon <nm@ti.com>
2011-07-31 16:26:16 -0500

    OMAP4460: OPP data: update voltages to spec rev 0.2
    Update voltages from Operating Conditions Data Manual
    Addendum Revison 0.2 (July 2011).
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -149,9 +149,9 @@ static struct omap_opp_def __initdata omap443x_opp_def_list[] = {
 };
 
 #define OMAP4460_VDD_MPU_OPP50_UV		1025000
-#define OMAP4460_VDD_MPU_OPP100_UV		1200000
-#define OMAP4460_VDD_MPU_OPPTURBO_UV		1313000
-#define OMAP4460_VDD_MPU_OPPNITRO_UV		1375000
+#define OMAP4460_VDD_MPU_OPP100_UV		1203000
+#define OMAP4460_VDD_MPU_OPPTURBO_UV		1317000
+#define OMAP4460_VDD_MPU_OPPNITRO_UV		1380000
 
 struct omap_volt_data omap446x_vdd_mpu_volt_data[] = {
 	VOLT_DATA_DEFINE(OMAP4460_VDD_MPU_OPP50_UV, OMAP44XX_CONTROL_FUSE_MPU_OPP50, 0xf4, 0x0c, OMAP_ABB_NOMINAL_OPP),
@@ -161,9 +161,9 @@ struct omap_volt_data omap446x_vdd_mpu_volt_data[] = {
 	VOLT_DATA_DEFINE(0, 0, 0, 0, 0),
 };
 
-#define OMAP4460_VDD_IVA_OPP50_UV		1025000
-#define OMAP4460_VDD_IVA_OPP100_UV		1200000
-#define OMAP4460_VDD_IVA_OPPTURBO_UV		1313000
+#define OMAP4460_VDD_IVA_OPP50_UV		 950000
+#define OMAP4460_VDD_IVA_OPP100_UV		1140000
+#define OMAP4460_VDD_IVA_OPPTURBO_UV		1291000
 #define OMAP4460_VDD_IVA_OPPNITRO_UV		1375000
 
 struct omap_volt_data omap446x_vdd_iva_volt_data[] = {
@@ -174,8 +174,8 @@ struct omap_volt_data omap446x_vdd_iva_volt_data[] = {
 	VOLT_DATA_DEFINE(0, 0, 0, 0, 0),
 };
 
-#define OMAP4460_VDD_CORE_OPP50_UV		1025000
-#define OMAP4460_VDD_CORE_OPP100_UV		1200000
+#define OMAP4460_VDD_CORE_OPP50_UV		 962000
+#define OMAP4460_VDD_CORE_OPP100_UV		1127000
 #define OMAP4460_VDD_CORE_OPP100_OV_UV		1250000
 
 struct omap_volt_data omap446x_vdd_core_volt_data[] = {
