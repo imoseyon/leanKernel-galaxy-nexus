a1c650586322a141e3536d612cee61ded25456da
Colin Cross <ccross@android.com>
2011-08-04 16:26:58 -0700

    ARM: omap: temperature_sensor: Fix build when temperature sensor disabled
    Provide stubs for omap_temp_sensor_resume_idle,
    omap_temp_sensor_prepare_idle and omap_temp_sensor_idle when
    CONFIG_OMAP_TEMP_SENSOR or CONFIG_OMAP_DIE_TEMP_SENSOR are
    not enabled.
    
    Change-Id: Ife9f9f619b2fad8469e18576db63f3d3f439aa8b
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/plat-omap/include/plat/temperature_sensor.h
+++ arch/arm/plat-omap/include/plat/temperature_sensor.h
@@ -48,8 +48,18 @@ struct omap_temp_sensor_pdata {
 	int irq;
 };
 
+#ifdef CONFIG_OMAP_TEMP_SENSOR
 void omap_temp_sensor_resume_idle(void);
 void omap_temp_sensor_prepare_idle(void);
+#else
+static inline void omap_temp_sensor_resume_idle(void) { }
+static inline void omap_temp_sensor_prepare_idle(void) { }
+#endif
+
+#ifdef CONFIG_OMAP_DIE_TEMP_SENSOR
 void omap_temp_sensor_idle(int idle_state);
+#else
+static inline void omap_temp_sensor_idle(int idle_state) { }
+#endif
 
 #endif
