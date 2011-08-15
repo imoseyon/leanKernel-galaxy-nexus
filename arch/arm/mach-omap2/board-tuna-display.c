1e8a968082869f997ec06809c895ffdf71ba2f78
Colin Cross <ccross@android.com>
2011-08-15 13:43:37 -0700

    ARM: omap4: tuna: Remove prelunchbox display support
    Change-Id: I68eba77112a251d313c3ea1edf15e8a1b1aa5f29
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/mach-omap2/board-tuna-display.c
+++ arch/arm/mach-omap2/board-tuna-display.c
@@ -31,23 +31,7 @@
 
 #define TUNA_FB_RAM_SIZE		SZ_16M /* ~1280*720*4 * 2 */
 
-#define TUNA_GPIO_MLCD_RST_LUNCHBOX	35
 #define TUNA_GPIO_MLCD_RST		23
-
-static struct panel_generic_dpi_data tuna_lcd_panel = {
-	.name			= "samsung_ams452gn05",
-	.platform_enable        = NULL,
-	.platform_disable       = NULL,
-};
-
-static struct omap_dss_device tuna_lcd_device = {
-	.name			= "lcd",
-	.driver_name		= "generic_dpi_panel",
-	.type			= OMAP_DISPLAY_TYPE_DPI,
-	.channel		= OMAP_DSS_CHANNEL_LCD2,
-	.data			= &tuna_lcd_panel,
-	.phy.dpi.data_lines	= 24,
-};
 
 struct regulator *tuna_oled_reg;
 
@@ -427,16 +411,6 @@ static struct omap_dss_board_info tuna_dss_data = {
 	.default_device	= &tuna_oled_device,
 };
 
-static struct omap_dss_device *prelunchbox_dss_devices[] = {
-	&tuna_lcd_device,
-};
-
-static struct omap_dss_board_info prelunchbox_dss_data = {
-	.num_devices	= ARRAY_SIZE(prelunchbox_dss_devices),
-	.devices	= prelunchbox_dss_devices,
-	.default_device	= &tuna_lcd_device,
-};
-
 static struct omapfb_platform_data tuna_fb_pdata = {
 	.mem_desc = {
 		.region_cnt = 1,
@@ -451,51 +425,8 @@ static struct omapfb_platform_data tuna_fb_pdata = {
 #define MUX_DISPLAY_OUT OMAP_PIN_OUTPUT | OMAP_MUX_MODE5
 void __init omap4_tuna_display_init(void)
 {
-	struct omap_dss_board_info *dss_data;
-
-	if (omap4_tuna_get_revision() == TUNA_REV_PRE_LUNCHBOX) {
-		/* dispc2_data23 - dispc2_data0 */
-		omap_mux_init_signal("usbb2_ulpitll_stp", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_dir", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_nxt", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_dat0", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_dat1", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_dat2", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu6", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu5", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_dat3", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_dat4", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_dat5", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_dat6", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("usbb2_ulpitll_dat7", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu3", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu4", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu11", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu12", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu13", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu14", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu15", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu16", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu17", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu18", MUX_DISPLAY_OUT);
-		omap_mux_init_signal("dpm_emu19", MUX_DISPLAY_OUT);
-		/* dispc2_hsync */
-		omap_mux_init_signal("dpm_emu7", MUX_DISPLAY_OUT);
-		/* dispc2_pclk */
-		omap_mux_init_signal("dpm_emu8", MUX_DISPLAY_OUT);
-		/* dispc2_vsync */
-		omap_mux_init_signal("dpm_emu9", MUX_DISPLAY_OUT);
-		/* dispc2_de */
-		omap_mux_init_signal("dpm_emu10", MUX_DISPLAY_OUT);
-
-		dss_data = &prelunchbox_dss_data;
-	} else {
-		omap4_ctrl_pad_writel(0x1FF80000, OMAP4_CTRL_MODULE_PAD_CORE_CONTROL_DSIPHY);
-		if (!omap4_tuna_final_gpios())
-			tuna_oled_data.reset_gpio = TUNA_GPIO_MLCD_RST_LUNCHBOX;
-		omap_mux_init_gpio(tuna_oled_data.reset_gpio, OMAP_PIN_OUTPUT);
-		dss_data = &tuna_dss_data;
-	}
+	omap4_ctrl_pad_writel(0x1FF80000, OMAP4_CTRL_MODULE_PAD_CORE_CONTROL_DSIPHY);
+	omap_mux_init_gpio(tuna_oled_data.reset_gpio, OMAP_PIN_OUTPUT);
 
 	if (omap4_tuna_get_revision() ==
 	    (omap4_tuna_get_type() == TUNA_TYPE_MAGURO ? 2 : 1)) {
@@ -514,5 +445,5 @@ void __init omap4_tuna_display_init(void)
 
 	omap_vram_set_sdram_vram(TUNA_FB_RAM_SIZE, 0);
 	omapfb_set_platform_data(&tuna_fb_pdata);
-	omap_display_init(dss_data);
+	omap_display_init(&tuna_dss_data);
 }
