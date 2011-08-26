3972071b60fead8dd5daba738dabdaa37e001bf1
PankajJindal <pankajjindal@ti.com>
2011-08-26 03:59:29 -0700

    ASoC: dapm: Prevent null codecs to affect card power level
    As described by commit ea77b94 ("ASoC: Fix power down for widgetless
    per-card DAPM context case"), non-codec DAPM contexts can force all
    DAPM contexts to active state, but not to power-down state. This same
    problem applies to null-codec used by ASoC core, so clearing the power
    state flag for null-codec before power widgets sequence.
    
    Change-Id: I447d31c63bd2189cb9c39c76d5cbff2bb7ff3ddc
    Signed-off-by: PankajJindal <pankajjindal@ti.com>
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -1498,7 +1498,8 @@ static int dapm_power_widgets(struct snd_soc_dapm_context *dapm, int event)
 	trace_snd_soc_dapm_start(card);
 
 	list_for_each_entry(d, &card->dapm_list, list)
-		if (d->n_widgets || d->codec == NULL)
+		if (d->n_widgets || d->codec == NULL ||
+				strstr(d->codec->name, "null-codec"))
 			d->dev_power = 0;
 
 	/* Check which widgets we need to power and store them in
