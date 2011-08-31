imoseyon/leanKernel-galaxy-nexus

commit 78ed87d0a8b3b003e8b0e4c2736550bee0eb2859

OMAP2+: PM: use off_mode_enabled as the global off mode flag
pm-interface already provides off_mode_enabled which when exposed
allows all platforms to depend on it instead of using PM_DEBUG
controlled enable_off_mode variable

Change-Id: I28795cf35ca59037ff0a2c788a12d20e9d3fa55f
Signed-off-by: Nishanth Menon <nm@ti.com>

arch/arm/mach-omap2/cpuidle34xx.c
@@ -157,7 +157,7 @@ static struct cpuidle_state *next_valid_state(struct cpuidle_device *dev,
 	u32 mpu_deepest_state = PWRDM_POWER_RET;
 	u32 core_deepest_state = PWRDM_POWER_RET;
 
-	if (enable_off_mode) {
+	if (off_mode_enabled) {
 		mpu_deepest_state = PWRDM_POWER_OFF;
 		/*
 		 * Erratum i583: valable for ES rev < Es1.2 on 3630.

arch/arm/mach-omap2/pm.h
@@ -72,13 +72,11 @@ extern struct omap_dm_timer *gptimer_wakeup;
 extern void omap2_pm_dump(int mode, int resume, unsigned int us);
 extern void omap2_pm_wakeup_on_timer(u32 seconds, u32 milliseconds);
 extern int omap2_pm_debug;
-extern u32 enable_off_mode;
 extern u32 sleep_while_idle;
 #else
 #define omap2_pm_dump(mode, resume, us)		do {} while (0);
 #define omap2_pm_wakeup_on_timer(seconds, milliseconds)	do {} while (0);
 #define omap2_pm_debug				0
-#define enable_off_mode 0
 #define sleep_while_idle 0
 #endif
 #ifdef CONFIG_PM_ADVANCED_DEBUG

arch/arm/plat-omap/include/plat/omap-pm.h
@@ -372,4 +372,6 @@ int omap_pm_set_min_mpu_freq(struct device *dev, unsigned long f);
 void omap_pm_enable_off_mode(void);
 void omap_pm_disable_off_mode(void);
 
+extern bool off_mode_enabled;
+
 #endif

arch/arm/plat-omap/omap-pm-interface.c
@@ -26,7 +26,7 @@
 
 #include "omap-pm-helper.h"
 
-static bool off_mode_enabled;
+bool off_mode_enabled;
 
 /*
  * Device-driver-originated constraints (via board-*.c files)

commit 4c4c7b5a1e7758e5b90d9ada703aac26f2117939

OMAP3: PM: CPUidle: include header for build breakage
Commit 78ed87d0a8b3b003e8b0e4c2736550bee0eb2859
(OMAP2+: PM: use off_mode_enabled as the global off mode flag)

introduced off_mode_enabled, however missed to include the required
header for the same. this causes breakage as follows:

 CC      arch/arm/mach-omap2/cpuidle34xx.o
 arch/arm/mach-omap2/cpuidle34xx.c: In function 'next_valid_state':
 arch/arm/mach-omap2/cpuidle34xx.c:160: error: 'off_mode_enabled'
 undeclared (first use in this function)

Change-Id: Id7361e6897b2dcd759c109edcf0f2890362fe590
Reported-by: Todd Poynor <toddpoynor@google.com>
Reported-by: Axel Haslam <axelhaslam@ti.com>
Signed-off-by: Nishanth Menon <nm@ti.com>

arch/arm/mach-omap2/cpuidle34xx.c
@@ -30,6 +30,7 @@
 #include "powerdomain.h"
 #include "clockdomain.h"
 #include <plat/serial.h>
+#include <plat/omap-pm.h>
 
 #include "pm.h"
 #include "control.h"