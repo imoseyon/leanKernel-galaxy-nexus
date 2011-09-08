418e9a59730e7c5ca7bc761b1f8f713977432430
Vikram Pandita <vikram.pandita@ti.com>
2011-09-07 20:36:20 -0700

    tty: serial: omap-serial: fix software flow control
    Software flow control register bits were not defined correctly.
    
    Also clarify the IXON and IXOFF logic to reflect what userspace wants.
    
    Change-Id: I8be2bf314e8b1370d9136f69b1298b45ac272094
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>

--- arch/arm/plat-omap/include/plat/omap-serial.h
+++ arch/arm/plat-omap/include/plat/omap-serial.h
@@ -40,10 +40,10 @@
 #define OMAP4_UART_WER_MOD_WKUP	0XFF
 
 /* Enable XON/XOFF flow control on output */
-#define OMAP_UART_SW_TX		0x04
+#define OMAP_UART_SW_TX		0x8
 
 /* Enable XON/XOFF flow control on input */
-#define OMAP_UART_SW_RX		0x04
+#define OMAP_UART_SW_RX		0x2
 
 #define OMAP_UART_SYSC_RESET	0X07
 #define OMAP_UART_TCR_TRIG	0X0F
