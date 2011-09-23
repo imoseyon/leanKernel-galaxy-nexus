41978441492eb365bb35a303f381aa2aec729b1b
Nishanth Menon <nm@ti.com>
2011-09-23 11:40:36 -0500

    OMAP2+: mux: warn only when signal really not found
    omap_mux_get_by_name calls _omap_mux_get_by_name for each of the
    partitions defined for the device while searching for the mux
    signal requested. However, on platforms such as OMAP4 which have
    multiple partitions, signal which is present in a later partition
    will be failed to be found by _omap_mux_get_by_name in the earlier
    partition search. It currently puts out a scary warning
    "_omap_mux_get_by_name: Could not find signal xyz"
    which is interpreted as mux configuration was faulty, which could
    be:
    a) a real mismatch due to typo error on behalf of the developer
    or
    b) just an indication that the signal was not found in an earlier
    partition.
    
    Instead move the warning to omap_mux_get_by_name to report this
    warning *only after* all alternatives have been exhausted to
    ensure that this warning is really for (a).
    
    Change-Id: Ia0c4e1e8f15877787cd2166a049f4990e5eb23e8
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/mach-omap2/mux.c
+++ arch/arm/mach-omap2/mux.c
@@ -210,8 +210,6 @@ static int __init _omap_mux_get_by_name(struct omap_mux_partition *partition,
 		return -EINVAL;
 	}
 
-	pr_err("%s: Could not find signal %s\n", __func__, muxname);
-
 	return -ENODEV;
 }
 
@@ -233,6 +231,8 @@ omap_mux_get_by_name(const char *muxname,
 
 		return mux_mode;
 	}
+
+	pr_err("%s: Could not find signal %s\n", __func__, muxname);
 
 	return -ENODEV;
 }
