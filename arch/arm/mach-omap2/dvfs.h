52095fae04d901b480231f9bab6c46410dffe54a
Nishanth Menon <nm@ti.com>
2011-09-30 15:58:24 -0500

    OMAP3+: PM: DVFS: Add api to check if any device is scaling
    Introduce omap_dvfs_is_any_dev_scaling which allows us to check
    if there was any device was in the middle of scale operation.
    
    NOTE: there are no locks used and the expectation is that this
    will be called in interrupt disabled context to ensure the right
    snapshot is taken
    
    Change-Id: Ifa48141e9d490a80428d89015e05970bbc223e81
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/mach-omap2/dvfs.h
+++ arch/arm/mach-omap2/dvfs.h
@@ -23,6 +23,7 @@ int omap_device_scale(struct device *req_dev, struct device *target_dev,
 		unsigned long rate);
 
 bool omap_dvfs_is_scaling(struct voltagedomain *voltdm);
+bool omap_dvfs_is_any_dev_scaling(void);
 #else
 static inline int omap_dvfs_register_device(struct device *dev,
 		char *voltdm_name, char *clk_name)
@@ -35,6 +36,10 @@ static inline int omap_device_scale(struct device *req_dev,
 	return -EINVAL;
 }
 static inline bool omap_dvfs_is_scaling(struct voltagedomain *voltdm)
+{
+	return false;
+}
+static inline bool omap_dvfs_is_any_dev_scaling(void)
 {
 	return false;
 }
