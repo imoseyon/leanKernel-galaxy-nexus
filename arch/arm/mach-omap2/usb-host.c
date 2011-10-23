937ae52f7a21d5a0a29deffd4cece5d42bcb2fd8
Vikram Pandita <vikram.pandita@ti.com>
2011-10-23 00:16:16 -0700

    arm: omap4: usb: disable pad wakeups
    Remote wakeup is not supported, so disable pad wake capability.
    This is to avoid cases when USBHOST can enter into INTRANSITION state,
    because of an un-expected remote-wakeup.
    
    Change-Id: I441a8ba0e165ab00fc59cdf6eae22cc09b2490ae
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>

--- arch/arm/mach-omap2/usb-host.c
+++ arch/arm/mach-omap2/usb-host.c
@@ -75,9 +75,7 @@ static struct omap_device_pad port1_phy_pads[] __initdata = {
 	},
 	{
 		.name = "usbb1_ulpitll_dir.usbb1_ulpiphy_dir",
-		.flags  = OMAP_DEVICE_PAD_REMUX | OMAP_DEVICE_PAD_WAKEUP,
-		.enable = (OMAP_PIN_INPUT_PULLDOWN | OMAP_MUX_MODE4) & ~OMAP_WAKEUP_EN,
-		.idle = OMAP_PIN_INPUT_PULLDOWN | OMAP_MUX_MODE4,
+		.enable = OMAP_PIN_INPUT_PULLDOWN | OMAP_MUX_MODE4,
 	},
 	{
 		.name = "usbb1_ulpitll_nxt.usbb1_ulpiphy_nxt",
@@ -85,9 +83,7 @@ static struct omap_device_pad port1_phy_pads[] __initdata = {
 	},
 	{
 		.name = "usbb1_ulpitll_dat0.usbb1_ulpiphy_dat0",
-		.flags  = OMAP_DEVICE_PAD_REMUX | OMAP_DEVICE_PAD_WAKEUP,
-		.enable = (OMAP_PIN_INPUT_PULLDOWN | OMAP_MUX_MODE4) & ~OMAP_WAKEUP_EN,
-		.idle = OMAP_PIN_INPUT_PULLDOWN | OMAP_MUX_MODE4,
+		.enable = OMAP_PIN_INPUT_PULLDOWN | OMAP_MUX_MODE4,
 	},
 	{
 		.name = "usbb1_ulpitll_dat1.usbb1_ulpiphy_dat1",
