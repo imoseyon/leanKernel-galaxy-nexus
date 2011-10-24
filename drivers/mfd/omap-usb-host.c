463da08327ced53306d9119740fd0d9ab2155604
Vikram Pandita <vikram.pandita@ti.com>
2011-10-24 14:02:49 -0700

    mfd: omap: fix hostconfig recommended configuration
    Recommendation is to put ENA_INCR_ALIGN == 1 for UHH_HOSTCONFIG
    Recommendation is to put APP_START_CLK  == 0 if OHCI is not used
    
    Fix this configuration
    
    Change-Id: Icd2f54dbd7a12dd5e2005d469f01658bfe202bb9
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>

--- drivers/mfd/omap-usb-host.c
+++ drivers/mfd/omap-usb-host.c
@@ -762,9 +762,8 @@ static void omap_usbhs_init(struct device *dev)
 	/* setup ULPI bypass and burst configurations */
 	reg |= (OMAP_UHH_HOSTCONFIG_INCR4_BURST_EN
 			| OMAP_UHH_HOSTCONFIG_INCR8_BURST_EN
-			| OMAP_UHH_HOSTCONFIG_INCR16_BURST_EN);
-	reg |= OMAP4_UHH_HOSTCONFIG_APP_START_CLK;
-	reg &= ~OMAP_UHH_HOSTCONFIG_INCRX_ALIGN_EN;
+			| OMAP_UHH_HOSTCONFIG_INCR16_BURST_EN
+			| OMAP_UHH_HOSTCONFIG_INCRX_ALIGN_EN);
 
 	if (is_omap_usbhs_rev1(omap)) {
 		if (pdata->port_mode[0] == OMAP_USBHS_PORT_MODE_UNUSED)
