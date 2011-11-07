408cb9a2afbb3911380ceeff39f02abbb8f0b9f1
Djamil Elaidi <d-elaidi@ti.com>
2011-11-07 15:09:46 +0100

    OMAP4: HSI: Fix for back to back CAWAKE interrupts
    If a CAWAKE interrupt occurs while previous CAWAKE is still being
    processed, the end of first CAWAKE processing will clear interrupt
    status bit for 1st CAWAKE interrupt, but also for second interrupt.
    So 2nd CAWAKE interrupt is missed.
    
    Added a backup CAWAKE interrupt (HSI_Pp_M_IRQrU_STATUS.HSR_WAKE) to take over
    the regular CAWAKE interrupt ((HSI_Pp_M_IRQr_STATUS.HSR_WAKE) while it is being
    processed.
    This CAWAKE backup interrupt will catch the second interrupt if it occurs.
    It guaranty we don't miss a CAWAKE interrupt while clearing the previous
    regular one.
    
    Change-Id: Id37bbe095f72af7dd478f8e9b1256d136b7ccebc
    Signed-off-by: Djamil Elaidi <d-elaidi@ti.com>
    Reported-by: Cedric Poignet <a0919860@ti.com>
    
    Conflicts:
    
    	drivers/omap_hsi/hsi_driver_int.c

--- drivers/omap_hsi/hsi_driver.h
+++ drivers/omap_hsi/hsi_driver.h
@@ -129,6 +129,7 @@ struct hsi_channel {
  * @cawake_gpio_irq: IRQ number for cawake gpio events
  * @cawake_status: Tracks CAWAKE line status
  * @cawake_off_event: True if CAWAKE event was detected from OFF mode
+ * @cawake_double_int: True if new CAWAKE detected while tasklet still executing
  * @acwake_status: Bitmap to track ACWAKE line status per channel
  * @in_int_tasklet: True if interrupt tasklet for this port is currently running
  * @in_cawake_tasklet: True if CAWAKE tasklet for this port is currently running
@@ -152,6 +153,7 @@ struct hsi_port {
 	int cawake_gpio_irq;
 	int cawake_status;
 	bool cawake_off_event;
+	bool cawake_double_int;
 	unsigned int acwake_status;	/* HSI_TODO : fine tune init values */
 	bool in_int_tasklet;
 	bool in_cawake_tasklet;
@@ -252,7 +254,9 @@ bool hsi_is_hsi_port_busy(struct hsi_port *pport);
 bool hsi_is_hsi_controller_busy(struct hsi_dev *hsi_ctrl);
 bool hsi_is_hst_port_busy(struct hsi_port *pport);
 bool hsi_is_hst_controller_busy(struct hsi_dev *hsi_ctrl);
-
+void hsi_driver_ack_interrupt(struct hsi_port *pport, u32 flag, bool backup);
+bool hsi_driver_is_interrupt_pending(struct hsi_port *pport, u32 flag,
+					bool backup);
 int hsi_driver_enable_interrupt(struct hsi_port *pport, u32 flag);
 int hsi_driver_disable_interrupt(struct hsi_port *pport, u32 flag);
 int hsi_driver_enable_read_interrupt(struct hsi_channel *hsi_channel,
