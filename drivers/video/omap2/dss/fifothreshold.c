da4e8af683538de32c5696c843fedc97cbe4e28a
Erik Gilling <konkers@android.com>
2011-11-14 18:15:01 -0800

    Revert "OMAP4: DSS2: Avoid VID/GFX underflow errors(v2)"
    This reverts commit 377eb2dcca8205ae30b1927bccac2b5c8c159a7f.

--- drivers/video/omap2/dss/fifothreshold.c
+++ drivers/video/omap2/dss/fifothreshold.c
@@ -367,9 +367,7 @@ static void sa_calc(struct dispc_config *dispc_reg_config, u32 channel_no,
 		 * When MemoryLineBufferSize > LineSize >
 		 * (MemoryLineBufferSize/2)
 		 */
-
-		/* HACK: we multiplied pict_16word by 2 as we hit underflow */
-		sa_info->min_sa = 2 * pict_16word + C2 * (Tot_mem -
+		sa_info->min_sa = pict_16word + C2 * (Tot_mem -
 						pict_16word_ceil - 8);
 	} else {
 		/* All other cases */
