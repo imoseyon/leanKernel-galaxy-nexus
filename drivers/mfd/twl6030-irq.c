c33a885e5fb472c8ab96260b5a76c36581332a51
Todd Poynor <toddpoynor@google.com>
2011-11-16 15:32:52 -0800

    HACK: mfd: twl6030: Don't disable BAT_VLOW interrupt after fired once
    We can get 3-4 of these in a row, but no need to disable it.
    
    Change-Id: I9226d7b5af5922e764d167bbab12da9871e8f509
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- drivers/mfd/twl6030-irq.c
+++ drivers/mfd/twl6030-irq.c
@@ -229,9 +229,7 @@ static irqreturn_t handle_twl6030_vlow(int irq, void *unused)
 	       2300 + (vbatmin_hi_threshold - 0b110) * 50);
 
 #if 1 /* temporary */
-	pr_err("%s: disabling BAT_VLOW interrupt\n", __func__);
-	disable_irq_nosync(twl6030_irq_base + TWL_VLOW_INTR_OFFSET);
-	WARN_ON(1);
+	WARN_ON_ONCE(1);
 #else
 	pr_emerg("handle_twl6030_vlow: kernel_power_off()\n");
 	kernel_power_off();
