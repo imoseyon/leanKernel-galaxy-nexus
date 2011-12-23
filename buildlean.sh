8e0ee0f8117dc5437f00689427168b16b9c27ca6
imoseyon <imoseyon@gmail.com>
2011-12-23 10:08:42 -0800

    some script and defconfig changes

--- buildlean.sh
+++ buildlean.sh
@@ -4,18 +4,20 @@ sed -i s/CONFIG_LOCALVERSION=\"-imoseyon-.*\"/CONFIG_LOCALVERSION=\"-imoseyon-${
 
 make -j2
 
+cp arch/arm/boot/zImage mkboot/
 cp .config arch/arm/configs/tuna_defconfig
 
 cd mkboot
 echo "making boot image"
 ./img.sh
 
+zipfile="imoseyon_leanKernel_v${1}ltenexus.zip"
 if [ ! $4 ]; then
 	echo "making zip file"
 	cp boot.img ../zip
 	cd ../zip
-	rm *.zip
+	rm -f *.zip
 	zip -r $zipfile *
-	rm /tmp/*.zip
+	rm -f /tmp/*.zip
 	cp *.zip /tmp
 fi
--- mkboot/img.sh
+++ mkboot/img.sh
@@ -1,5 +1,3 @@
 echo "making ramdisk"
 ./mkbootfs boot.img-ramdisk | gzip > newramdisk.cpio.gz
 ./mkbootimg --cmdline 'no_console_suspend=1 console=null' --kernel zImage --ramdisk newramdisk.cpio.gz -o boot.img 
-echo "copying files to /tmp"
-cp boot.img /tmp
