ebef68d6e1089a72aff32d84e1e7747099606b69
imoseyon <imoseyon@gmail.com>
2011-12-24 17:20:48 -0800

    Keep boot min freq at 350Mhz, in case i create a lower slot   in the future.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -946,6 +946,10 @@ static int cpufreq_add_dev(struct sys_device *sys_dev)
 		pr_info("[imoseyon] cpufreq policy max set to 1.2Ghz at boot");
 		policy->max = 1200000;
 	}
+	if (policy->min < 350000) {
+		pr_info("[imoseyon] cpufreq policy min set to 350Mhz at boot");
+		policy->min = 350000;
+	}
 
 	policy->user_policy.min = policy->min;
 	policy->user_policy.max = policy->max;
