8108eb8c4a95bef1ed82150b08844162c7b92869
imoseyon <imoseyon@gmail.com>
2011-12-26 19:23:42 -0800

    build script changes to prevent defconfig file from changing and zip file name change

--- buildlean.sh
+++ buildlean.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-sed -i s/CONFIG_LOCALVERSION=\"-imoseyon-.*\"/CONFIG_LOCALVERSION=\"-imoseyon-${1}\"/ .config
+sed -i s/CONFIG_LOCALVERSION=\".*\"/CONFIG_LOCALVERSION=\"-imoseyon-${1}\"/ .config
 
 make -j2
 
 cp arch/arm/boot/zImage mkboot/
+sed -i s/CONFIG_LOCALVERSION=\".*\"/CONFIG_LOCALVERSION=\"\"/ .config
 cp .config arch/arm/configs/tuna_defconfig
 
 cd mkboot
 echo "making boot image"
 ./img.sh
 
-zipfile="imoseyon_leanKernel_v${1}ltenexus.zip"
+zipfile="imoseyon_leanKernel_v${1}gnexus.zip"
 if [ ! $4 ]; then
 	echo "making zip file"
 	cp boot.img ../zip
