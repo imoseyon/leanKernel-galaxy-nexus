231a6e9f2e9307aadbdccb7ed56e62fd95759287
Mahesh Renduchintala <mahesh@ti.com>
2012-01-04 23:52:43 -0600

    omap: dispc: force L3_2 CD to NOSLEEP when dispc module is active
    It has been identified that in addition to L3_1, L3_2 clock domain is
    also suspetible to the erratum  (erratum ID is pending) described below.
    
    Issue: Mstandby and disconnect protocol issue
    impacts: all OMAP4 devices
    Simplfied Description:
    issue #1: The handshake between IP modules on L3_1 and L3_2 peripherals with
    PRCM has a limitation in a certain time window of L4 clock cycle. Due to the
    fact that a wrong variant of stall signal was used in circuit of PRCM, the
    intitator-interconnect protocol is broken when the time window is hit where
    the PRCM requires the interconnect to go to idle while intitator asks to
    wakeup.
    Issue #2: DISPC asserts a sub-mstandby signal for a short period. In this
    time interval, IP block requests disconnection of Master port, and results
    in Mstandby and wait request to PRCM. In parallel, if mstandby is de-asserted
    by DISPC simultaneously, interconnect requests for a reconnect for one cycle
    alone resulting in a disconnect protocol violation and a deadlock of the
    system.
    
    Workaround:
    L3_1 clock domain must not be programmed in HW_AUTO if
    Static dependency with DSS is enabled and DSS clock domain
    is ON. Same for L3_2.
    
    Patch with changeID I28ae362ad330a79a493927575c9570462c4303a7
    (omap: dispc: force L3_1 CD to NOSLEEP when dispc module is active)
    already introduces the work around for L3_1. Here we add L3_2
    clock domain as well + update the comments and wrap the WA under
    an OMAP4 flag.
    
    Change-Id: I476eedbac34e1876d413a15a524c3b4bb3732b9f
    Signed-off-by: Akash Choudhari <akashc@ti.com>
    Signed-off-by: Mahesh Renduchintala <mahesh@ti.com>
    CC: Todd Poynor <toddpoynor@google.com>
    CC: Colin Cross <ccross@android.com>
    CC: Nishanth Menon <nm@ti.com>
    CC: Girish S G <girishsg@ti.com>
    CC: Dandawate Saket <dsaket@ti.com>
    CC: Lajos Molnar <molnar@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -60,7 +60,7 @@
 
 #define DISPC_MAX_NR_ISRS		8
 
-static struct clockdomain *l3_1_clkdm;
+static struct clockdomain *l3_1_clkdm, *l3_2_clkdm;
 
 struct omap_dispc_isr_data {
 	omap_dispc_isr_t	isr;
@@ -529,13 +529,33 @@ int dispc_runtime_get(void)
 		DSSDBG("dispc_runtime_get\n");
 
 		/*
-		 * With the DSS FIFO optimizations, ramdom lockups and reboots
-		 * are seen. It has been identified that L3_1 CD is idling and
-		 * not responding to the traffic initiated by DSS.  The
-		 * Workaround suggested by Hardware team is to keep the L3_1
-		 * CD in NO_SLEEP mode, when DSS is active.
+		 * OMAP4 ERRATUM xxxx: Mstandby and disconnect protocol issue
+		 * Impacts: all OMAP4 devices
+		 * Simplfied Description:
+		 * issue #1: The handshake between IP modules on L3_1 and L3_2
+		 * peripherals with PRCM has a limitation in a certain time
+		 * window of L4 clock cycle. Due to the fact that a wrong
+		 * variant of stall signal was used in circuit of PRCM, the
+		 * intitator-interconnect protocol is broken when the time
+		 * window is hit where the PRCM requires the interconnect to go
+		 * to idle while intitator asks to wakeup.
+		 * Issue #2: DISPC asserts a sub-mstandby signal for a short
+		 * period. In this time interval, IP block requests
+		 * disconnection of Master port, and results in Mstandby and
+		 * wait request to PRCM. In parallel, if mstandby is de-asserted
+		 * by DISPC simultaneously, interconnect requests for a
+		 * reconnect for one cycle alone resulting in a disconnect
+		 * protocol violation and a deadlock of the system.
+		 *
+		 * Workaround:
+		 * L3_1 clock domain must not be programmed in HW_AUTO if
+		 * Static dependency with DSS is enabled and DSS clock domain
+		 * is ON. Same for L3_2.
 		 */
-		clkdm_deny_idle(l3_1_clkdm);
+		if (cpu_is_omap44xx()) {
+			clkdm_deny_idle(l3_1_clkdm);
+			clkdm_deny_idle(l3_2_clkdm);
+		}
 
 		r = dss_runtime_get();
 		if (r)
@@ -584,10 +604,14 @@ void dispc_runtime_put(void)
 		dss_runtime_put();
 
 		/*
-		 * Restore the L3_1 CD to HW_AUTO, when DSS module idles.
-		 * When DSS is idle, we can allow L3_1 to idle.
+		 * OMAP4 ERRATUM xxxx: Mstandby and disconnect protocol issue
+		 * Workaround:
+		 * Restore L3_1 amd L3_2 CD to HW_AUTO, when DSS module idles.
 		 */
-		clkdm_allow_idle(l3_1_clkdm);
+		if (cpu_is_omap44xx()) {
+			clkdm_allow_idle(l3_1_clkdm);
+			clkdm_allow_idle(l3_2_clkdm);
+		}
 
 	}
 
@@ -3983,7 +4007,11 @@ static void _omap_dispc_initial_config(void)
 		dispc_write_reg(DISPC_DIVISOR, l);
 	}
 
-	l3_1_clkdm = clkdm_lookup("l3_1_clkdm");
+	/* for OMAP4 ERRATUM xxxx: Mstandby and disconnect protocol issue */
+	if (cpu_is_omap44xx()) {
+		l3_1_clkdm = clkdm_lookup("l3_1_clkdm");
+		l3_2_clkdm = clkdm_lookup("l3_2_clkdm");
+	}
 
 	/* FUNCGATED */
 	if (dss_has_feature(FEAT_FUNCGATED))
