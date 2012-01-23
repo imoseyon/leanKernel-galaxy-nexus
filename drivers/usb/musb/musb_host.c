159f916e819405fa2399a73763e3dbc117cf1a11
Benoit Goby <benoit@android.com>
2012-01-23 11:49:52 -0800

    Revert "usb: musb: Add a BUG_ON if trying to suspend musb in B_IDLE state"
    This reverts commit 8ebb62bd38adcbef29a457a064f61c1d9bb5526c.

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -2290,7 +2290,6 @@ static int musb_bus_suspend(struct usb_hcd *hcd)
 	if (musb->is_active) {
 		WARNING("trying to suspend as %s while active\n",
 				otg_state_string(musb->xceiv->state));
-		BUG();
 		return -EBUSY;
 	} else
 		return 0;
