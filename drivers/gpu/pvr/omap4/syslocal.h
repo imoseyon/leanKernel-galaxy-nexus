5ef25e04d96c3e218e811bc5613b7bcb4f6d5d0d
Alistair Strachan <alistair.strachan@imgtec.com>
2012-01-27 12:14:44 +0000

    gpu: pvr: Update to DDK 1.8@796887.
     - Re-introduce DC refcounting fix, which makes an unrelated (and
       unfixed) problem more reproducible.
     - Fix a bug where the driver would not wait for propagation (during
       power transitions) before performing a soft reset.
    
    Change-Id: I88d9b6d0e434bcbcfacae967b634a0699723d35b

--- drivers/gpu/pvr/omap4/syslocal.h
+++ drivers/gpu/pvr/omap4/syslocal.h
@@ -82,10 +82,8 @@
 #endif
 
 #if defined(__linux__)
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,35)) && defined(SGX_OCP_REGS_ENABLED)
-#if !defined(SGX544)
+#if defined(SGX_OCP_REGS_ENABLED)
 #define SGX_OCP_NO_INT_BYPASS
-#endif
 #endif
 #endif
 
