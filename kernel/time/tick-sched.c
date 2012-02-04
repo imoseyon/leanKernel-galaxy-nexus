f8de4e20977dbb14f8c2c8698e3fadc929b278ca
imoseyon <imoseyon@gmail.com>
2012-02-03 22:22:42 -0800

    Revert "i think we can do without these cluttering up dmesg."
    This reverts commit 984faa0e1d51ffd3e506d5de3b0e522c49deb6fe.

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -641,7 +641,7 @@ static void tick_nohz_switch_to_nohz(void)
 	}
 	local_irq_enable();
 
-//	printk(KERN_INFO "Switched to NOHz mode on CPU #%d\n", smp_processor_id());
+	printk(KERN_INFO "Switched to NOHz mode on CPU #%d\n", smp_processor_id());
 }
 
 /*
@@ -795,7 +795,7 @@ void tick_setup_sched_timer(void)
 #ifdef CONFIG_NO_HZ
 	if (tick_nohz_enabled) {
 		ts->nohz_mode = NOHZ_MODE_HIGHRES;
-//		printk(KERN_INFO "Switched to NOHz mode on CPU #%d\n", smp_processor_id());
+		printk(KERN_INFO "Switched to NOHz mode on CPU #%d\n", smp_processor_id());
 	}
 #endif
 }
