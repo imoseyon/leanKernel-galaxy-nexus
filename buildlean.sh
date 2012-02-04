87ece48090405b0d9a37a6d83243db7861d97d69
imoseyon <imoseyon@gmail.com>
2012-02-04 08:15:59 -0800

    build script: to support lkflash

--- buildlean.sh
+++ buildlean.sh
@@ -23,3 +23,6 @@ if [ ! $4 ]; then
 	rm -f /tmp/*.zip
 	cp *.zip /tmp
 fi
+md5=`md5sum /tmp/boot.img | awk '{ print \$1 }'`
+cp /tmp/boot.img /tmp/boot-${1}.img
+echo "http://imoseyon.host4droid.com/boot-${1}.img $md5 ${1}" > /tmp/latest$1
