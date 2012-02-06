785abbbe01983d52eede29088f3c703ef0d324ee
imoseyon <imoseyon@gmail.com>
2012-02-05 17:39:56 -0800

    fs: add CONFIG_FILE_SYNC_DISABLE
    fsync disable patch by arighi, modified by me to work on Linux 3.x.

--- fs/sync.c
+++ fs/sync.c
@@ -129,6 +129,17 @@ void emergency_sync(void)
 	}
 }
 
+#ifdef CONFIG_FILE_SYNC_DISABLE
+static inline int do_fsync(unsigned int fd, int datasync)
+{
+	return 0;
+}
+SYSCALL_DEFINE1(syncfs, int, fd)
+{
+	return 0;
+}
+
+#else /* !CONFIG_FILE_SYNC_DISABLE */
 /*
  * sync a single super
  */
@@ -190,6 +201,20 @@ out:
 }
 EXPORT_SYMBOL(vfs_fsync_range);
 
+static int do_fsync(unsigned int fd, int datasync)
+{
+	struct file *file;
+	int ret = -EBADF;
+
+	file = fget(fd);
+	if (file) {
+		ret = vfs_fsync(file, datasync);
+		fput(file);
+	}
+	return ret;
+}
+#endif /* CONFIG_FILE_SYNC_DISABLE */
+
 /**
  * vfs_fsync - perform a fsync or fdatasync on a file
  * @file:		file to sync
@@ -203,19 +228,6 @@ int vfs_fsync(struct file *file, int datasync)
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
-
-static int do_fsync(unsigned int fd, int datasync)
-{
-	struct file *file;
-	int ret = -EBADF;
-
-	file = fget(fd);
-	if (file) {
-		ret = vfs_fsync(file, datasync);
-		fput(file);
-	}
-	return ret;
-}
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
@@ -244,6 +256,13 @@ int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 }
 EXPORT_SYMBOL(generic_write_sync);
 
+#ifdef CONFIG_FILE_SYNC_DISABLE
+SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
+				unsigned int flags)
+{
+	return 0;
+}
+#else /* !CONFIG_FILE_SYNC_DISABLE */
 /*
  * sys_sync_file_range() permits finely controlled syncing over a segment of
  * a file in the range offset .. (offset+nbytes-1) inclusive.  If nbytes is
@@ -374,6 +393,8 @@ out_put:
 out:
 	return ret;
 }
+#endif /* CONFIG_FILE_SYNC_DISABLE */
+
 #ifdef CONFIG_HAVE_SYSCALL_WRAPPERS
 asmlinkage long SyS_sync_file_range(long fd, loff_t offset, loff_t nbytes,
 				    long flags)
--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2177,8 +2177,16 @@ extern int __filemap_fdatawrite_range(struct address_space *mapping,
 extern int filemap_fdatawrite_range(struct address_space *mapping,
 				loff_t start, loff_t end);
 
+#ifdef CONFIG_FILE_SYNC_DISABLE
+static inline int
+vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
+{
+	return 0;
+}
+#else
 extern int vfs_fsync_range(struct file *file, loff_t start, loff_t end,
 			   int datasync);
+#endif
 extern int vfs_fsync(struct file *file, int datasync);
 extern int generic_write_sync(struct file *file, loff_t pos, loff_t count);
 extern void sync_supers(void);
