8f3edac66643ab8d87530d0f14180013aa15d0ed
imoseyon <imoseyon@gmail.com>
2012-02-22 15:37:04 -0800

    omap2plus-cpufreq: set transition latency to 30ms
    Vishwanath says 30ms is best, so I'm gonna agree with him:
    http://www.mail-archive.com/linaro-dev@lists.linaro.org/msg01373.html

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -361,7 +361,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 	}
 
 	/* FIXME: what's the actual transition time? */
-	policy->cpuinfo.transition_latency = 300 * 1000;
+	policy->cpuinfo.transition_latency = 30 * 1000;
 
 	return 0;
 
