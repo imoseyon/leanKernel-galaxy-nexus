a66a12cc2adc0bd8ccee87c7602e048422068a27
imoseyon <imoseyon@gmail.com>
2012-04-18 16:14:43 -0700

    SR15: skip calibration on higher freqs - take two

--- arch/arm/mach-omap2/smartreflex-class1p5.c
+++ arch/arm/mach-omap2/smartreflex-class1p5.c
@@ -427,6 +427,14 @@ static int sr_class1p5_enable(struct voltagedomain *voltdm,
 	/* If already calibrated, nothing to do here.. */
 	if (volt_data->volt_calibrated)
 		return 0;
+        // SR1.5 does not seem to calibrate high freqs properly - this is a
+        // workaround until I find a better way
+ 	if (volt_data->volt_nominal > 1310000) {
+		pr_info("[imoseyon] nominal@%d, skipping sr_enable\n", 
+			volt_data->volt_nominal);
+		volt_data->volt_calibrated = volt_data->volt_nominal;
+		return 0;
+	}
 
 	work_data = (struct sr_class1p5_work_data *)voltdm_cdata;
 	if (IS_ERR_OR_NULL(work_data)) {
