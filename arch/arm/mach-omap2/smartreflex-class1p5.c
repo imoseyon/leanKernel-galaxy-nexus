2b977983895275620568dbaa2c93917bd1c7ed91
imoseyon <imoseyon@gmail.com>
2012-04-19 12:33:10 -0700

    SR1.5: just to complete the calibration tweak for high freq/voltage

--- arch/arm/mach-omap2/smartreflex-class1p5.c
+++ arch/arm/mach-omap2/smartreflex-class1p5.c
@@ -433,6 +433,7 @@ static int sr_class1p5_enable(struct voltagedomain *voltdm,
 		pr_info("[imoseyon] nominal@%d, skipping sr_enable\n", 
 			volt_data->volt_nominal);
 		volt_data->volt_calibrated = volt_data->volt_nominal;
+		volt_data->volt_dynamic_nominal = volt_data->volt_nominal;
 		return 0;
 	}
 
