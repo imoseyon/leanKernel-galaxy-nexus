74b5b12d2175d22144bf9876b3b2cff8afb7c8e3
imoseyon <imoseyon@gmail.com>
2012-04-26 09:42:33 -0700

    OMAPDSS: DSSCOMP: efficiency improvement.
    This change adds 3 local caches of objects, which are used at each flip buffer
    action. In original variant kzalloc()/kfree() functions are called each time
    dsscomp_gralloc_queue() function is called. This meens that slab|slob|slub
    common cache is used at each frame draw action. Call to kzalloc()/kfree()
    result in synchronization with other code, which uses common cache, and can
    result in dips in FPS, when other code allocates/frees memory.
    This change improves reliability, autonomy and efficiency of DSSCOMP module.
    
    Change-Id: Ibf729b922fcd613ff2a2da8e452e0075f5397e55
    Signed-off-by: Mykhailo Denysiuk <x0172934@ti.com>
    
    Patch Set 16

--- drivers/video/omap2/dsscomp/gralloc.c
+++ drivers/video/omap2/dsscomp/gralloc.c
@@ -38,6 +38,9 @@ struct dsscomp_gralloc_t {
 	bool programmed;
 };
 
+/* local cache */
+static struct kmem_cache *gsync_cachep;
+
 /* queued gralloc compositions */
 static LIST_HEAD(flip_queue);
 
@@ -102,7 +105,8 @@ static void dsscomp_gralloc_cb(void *data, int status)
 
 		if (gsync->cb_fn)
 			gsync->cb_fn(gsync->cb_arg, 1);
-		kfree(gsync);
+
+		kmem_cache_free(gsync_cachep, gsync);
 	}
 }
 
@@ -177,8 +181,16 @@ int dsscomp_gralloc_queue(struct dsscomp_setup_dispc_data *d,
 
 	mutex_lock(&mtx);
 
-	/* create sync object with 1 temporary ref */
-	gsync = kzalloc(sizeof(*gsync), GFP_KERNEL);
+	/* allocate sync object with 1 temporary ref */
+	gsync = kmem_cache_zalloc(gsync_cachep, GFP_KERNEL);
+	if (!gsync) {
+		mutex_unlock(&mtx);
+		mutex_unlock(&local_mtx);
+		pr_err("DSSCOMP: %s: can't allocate object from cache\n",
+								__func__);
+		return -ENOMEM;
+	}
+
 	gsync->cb_arg = cb_arg;
 	gsync->cb_fn = cb_fn;
 	gsync->refs.counter = 1;
@@ -587,6 +599,15 @@ void dsscomp_gralloc_init(struct dsscomp_dev *cdev_)
 		/* reset free_slots if no TILER memory could be reserved */
 		if (!i)
 			ZERO(free_slots);
+	}
+
+	/* create cache at first time */
+	if (!gsync_cachep) {
+		gsync_cachep = kmem_cache_create("gsync_cache",
+						sizeof(struct dsscomp_gralloc_t), 0,
+						SLAB_HWCACHE_ALIGN, NULL);
+		if (!gsync_cachep)
+			pr_err("DSSCOMP: %s: can't create cache\n", __func__);
 	}
 }
 
--- drivers/video/omap2/dsscomp/queue.c
+++ drivers/video/omap2/dsscomp/queue.c
@@ -99,6 +99,21 @@ static void maskref_decmask(struct maskref *om, u32 mask)
  * ===========================================================================
  */
 
+struct dsscomp_cb_work {
+	struct work_struct work;
+	struct dsscomp_data *comp;
+	int status;
+};
+
+struct dsscomp_apply_work {
+	struct work_struct work;
+	dsscomp_t comp;
+};
+
+/* Local caches */
+static struct kmem_cache *dsscomp_cb_wk_cachep;
+static struct kmem_cache *dsscomp_app_wk_cachep;
+
 /* Initialize queue structures, and set up state of the displays */
 int dsscomp_queue_init(struct dsscomp_dev *cdev_)
 {
@@ -128,6 +143,31 @@ int dsscomp_queue_init(struct dsscomp_dev *cdev_)
 	cb_wkq = create_singlethread_workqueue("dsscomp_cb");
 	if (!cb_wkq)
 		goto error;
+
+	/* create cache for dsscomp_cb_work structures */
+	if (!dsscomp_cb_wk_cachep) {
+		dsscomp_cb_wk_cachep = kmem_cache_create("cb_wk_cache",
+						sizeof(struct dsscomp_cb_work), 0,
+						SLAB_HWCACHE_ALIGN, NULL);
+		if (!dsscomp_cb_wk_cachep) {
+			pr_err("DSSCOMP: %s: can't create cache\n",
+							__func__);
+			goto error;
+		}
+	}
+
+	/* create cache for dsscomp_apply_work structures */
+	if (!dsscomp_app_wk_cachep) {
+		dsscomp_app_wk_cachep = kmem_cache_create("app_wk_cache",
+						sizeof(struct dsscomp_apply_work), 0,
+						SLAB_HWCACHE_ALIGN, NULL);
+		if (!dsscomp_app_wk_cachep) {
+			pr_err("DSSCOMP: %s: can't create cache\n", __func__);
+			/* destroy previously created cache */
+			kmem_cache_destroy(dsscomp_cb_wk_cachep);
+			goto error;
+		}
+	}
 
 	return 0;
 error:
@@ -369,12 +409,6 @@ void dsscomp_drop(dsscomp_t comp)
 }
 EXPORT_SYMBOL(dsscomp_drop);
 
-struct dsscomp_cb_work {
-	struct work_struct work;
-	struct dsscomp_data *comp;
-	int status;
-};
-
 static void dsscomp_mgr_delayed_cb(struct work_struct *work)
 {
 	struct dsscomp_cb_work *wk = container_of(work, typeof(*wk), work);
@@ -382,7 +416,7 @@ static void dsscomp_mgr_delayed_cb(struct work_struct *work)
 	int status = wk->status;
 	u32 ix;
 
-	kfree(work);
+	kmem_cache_free(dsscomp_cb_wk_cachep, wk);
 
 	mutex_lock(&mtx);
 
@@ -429,7 +463,16 @@ static u32 dsscomp_mgr_callback(void *data, int id, int status)
 	    (status == DSS_COMPLETION_DISPLAYED &&
 	     comp->state != DSSCOMP_STATE_DISPLAYED) ||
 	    (status & DSS_COMPLETION_RELEASED)) {
-		struct dsscomp_cb_work *wk = kzalloc(sizeof(*wk), GFP_ATOMIC);
+		struct dsscomp_cb_work *wk;
+
+		/* allocate work object from cache */
+		wk = kmem_cache_zalloc(dsscomp_cb_wk_cachep, GFP_ATOMIC);
+		if (!wk) {
+			pr_err("DSSCOMP: %s: can't allocate cache object\n",
+								__func__);
+			return ~status;
+		}
+
 		wk->comp = comp;
 		wk->status = status;
 		INIT_WORK(&wk->work, dsscomp_mgr_delayed_cb);
@@ -631,11 +674,6 @@ done:
 	return r;
 }
 
-struct dsscomp_apply_work {
-	struct work_struct work;
-	dsscomp_t comp;
-};
-
 int dsscomp_state_notifier(struct notifier_block *nb,
 						unsigned long arg, void *ptr)
 {
@@ -662,15 +700,21 @@ static void dsscomp_do_apply(struct work_struct *work)
 	/* complete compositions that failed to apply */
 	if (dsscomp_apply(wk->comp))
 		dsscomp_mgr_callback(wk->comp, -1, DSS_COMPLETION_ECLIPSED_SET);
-	kfree(wk);
+	kmem_cache_free(dsscomp_app_wk_cachep, wk);
 }
 
 int dsscomp_delayed_apply(dsscomp_t comp)
 {
 	/* don't block in case we are called from interrupt context */
-	struct dsscomp_apply_work *wk = kzalloc(sizeof(*wk), GFP_NOWAIT);
-	if (!wk)
+	struct dsscomp_apply_work *wk;
+
+	/* allocate work object from cache */
+	wk = kmem_cache_zalloc(dsscomp_app_wk_cachep, GFP_NOWAIT);
+	if (!wk) {
+		pr_err("DSSCOMP: %s: can't allocate object from cache\n",
+								__func__);
 		return -ENOMEM;
+	}
 
 	mutex_lock(&mtx);
 
