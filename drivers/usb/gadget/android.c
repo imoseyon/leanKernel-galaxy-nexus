32cfe30dcc13f5f38c523b783e319e1084edd386
Moiz Sonasath <m-sonasath@ti.com>
2012-04-26 17:46:06 -0500

    usb:gadget:android: avoid sending extra uevents
    Don't schedule a uevet from android_disconnect
    if there is no change in the connection state
    and if the DISCONNECT event was already sent.
    This patch will avoid these unecessary messages:
    
    android_work: did not send uevent (0 0   (null))
    
    Change-Id: Ic25e09f24098dcc575f4a24fdb0a0ec49ad0f430
    Signed-off-by: Moiz Sonasath <m-sonasath@ti.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1107,8 +1107,10 @@ static void android_disconnect(struct usb_gadget *gadget)
 	composite_disconnect(gadget);
 
 	spin_lock_irqsave(&cdev->lock, flags);
-	dev->connected = 0;
-	schedule_work(&dev->work);
+	if (dev->connected) {
+		dev->connected = 0;
+		schedule_work(&dev->work);
+	}
 	spin_unlock_irqrestore(&cdev->lock, flags);
 }
 
