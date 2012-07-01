a150082ce4eaac25a2e56f5ab25814b72a7ab61c
imoseyon <imoseyon@gmail.com>
2012-07-01 09:50:01 -0700

    ramdisk: fix tunapower.so from init.tuna.rc

--- mkboot/boot.img-ramdisk/sbin/fixtunapower.sh
+++ mkboot/boot.img-ramdisk/sbin/fixtunapower.sh
@@ -3,8 +3,8 @@ bb="/sbin/bb/busybox"
 md5=`$bb md5sum /system/lib/hw/power.tuna.so | $bb awk '{ print $1 }'`
 [[ $md5 == "a70ff42a1f19c6bc965acbbdbe4bfb3c" ]] && exit
 
-mount -o rw,remount /system
+$bb mount -o rw,remount /system
 $bb cp /system/lib/hw/power.tuna.so /system/lib/hw/power.tuna.so.bak
 $bb cp /sbin/init.d/power.tuna.so /system/lib/hw
-$bb chmod 644 /system/ilb/hw/power.tuna.so
-mount -o ro,remount /system
+$bb chmod 644 /system/lib/hw/power.tuna.so
+$bb mount -o ro,remount /system
