9149ef1ce0f5a059b4cae2bfc874796e16fe3a76
imoseyon <imoseyon@gmail.com>
2012-07-21 23:38:05 -0700

    mkboot: more permission fixes

--- mkboot/img.sh
+++ mkboot/img.sh
@@ -4,6 +4,7 @@ cd boot.img-ramdisk
 chmod 750 init* charger
 chmod 644 default.prop
 chmod 640 fstab.tuna
+chmod 644 ueventd*
 find . | cpio -o -H newc | gzip > ../newramdisk.cpio.gz
 cd ..
 ./mkbootimg --cmdline 'no_console_suspend=1 console=null' --kernel zImage --ramdisk newramdisk.cpio.gz -o boot.img 
