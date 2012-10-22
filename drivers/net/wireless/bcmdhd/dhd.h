e94a38b9c9b8d1e9237ccff869f178f3bc3d9c3b
Dmitry Shmidt <dimitrysh@google.com>
2012-10-22 14:18:27 -0700

    net: wireless: bcmdhd: Reduce keep-alive period to 28 sec
    Bug: 7386287
    
    Change-Id: I115135e1d2cba0135c5201b95a2599dcabac0247
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    Conflicts:
    
    	drivers/net/wireless/bcmdhd/dhd.h

--- drivers/net/wireless/bcmdhd/dhd.h
+++ drivers/net/wireless/bcmdhd/dhd.h
@@ -594,8 +594,8 @@ extern uint dhd_sdiod_drive_strength;
 
 /* Override to force tx queueing all the time */
 extern uint dhd_force_tx_queueing;
-/* Default KEEP_ALIVE Period is 55 sec to prevent AP from sending Keep Alive probe frame */
-#define KEEP_ALIVE_PERIOD 55000
+/* Default KEEP_ALIVE Period is 28 sec to prevent AP from sending Keep Alive probe frame */
+#define KEEP_ALIVE_PERIOD 28000
 #define NULL_PKT_STR	"null_pkt"
 
 #ifdef SDTEST
